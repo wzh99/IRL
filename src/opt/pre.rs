9dd3f32b6e5d3c6787e2e27c0a56a561165453c5
wzh99 <wzh1999_frog@126.com>
2020-02-29 18:24:51 +0800

    First phase of set construction.

--- src/opt/pre.rs
+++ src/opt/pre.rs
@@ -1,10 +1,12 @@
-use std::collections::{BTreeSet, HashMap, HashSet};
+use std::cell::RefCell;
+use std::collections::{BTreeSet, HashMap};
 use std::hash::{Hash, Hasher};
+use std::ops::Deref;
 
-use crate::lang::func::{BlockRef, Func};
-use crate::lang::instr::{BinOp, UnOp};
+use crate::lang::func::{BlockListener, BlockRef, Func};
+use crate::lang::instr::{BinOp, Instr};
 use crate::lang::Program;
-use crate::lang::value::{Const, SymbolRef, Type};
+use crate::lang::value::{Const, SymbolRef, Type, Typed, Value};
 use crate::opt::{FnPass, Pass};
 use crate::opt::gvn::Gvn;
 
@@ -12,36 +14,10 @@ use crate::opt::gvn::Gvn;
 enum Expr {
     /// Compile time constants
     Const(Const),
-    /// Unary operations
-    Unary(UnExpr),
     /// Binary operations
     Binary(BinExpr),
     /// Temporaries that store values
     Temp(SymbolRef),
-    /// Any operation not in consideration for this optimization, including global variables and
-    /// results of neither unary or binary operations. Pointer arithmetic is also in this category,
-    /// since its has arbitrary operands.
-    Result(SymbolRef),
-}
-
-#[derive(Eq, Clone, Debug)]
-struct UnExpr {
-    op: UnOp,
-    ty: Type,
-    opd: usize, // value number
-}
-
-impl PartialEq for UnExpr {
-    fn eq(&self, other: &Self) -> bool {
-        self.op == other.op && self.opd == other.opd
-    }
-}
-
-impl Hash for UnExpr {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.op.hash(state);
-        self.opd.hash(state);
-    }
 }
 
 #[derive(Eq, Clone, Debug)]
@@ -67,12 +43,13 @@ impl Hash for BinExpr {
     }
 }
 
+#[derive(Debug)]
 struct ValueTable {
     /// Expression list for each numbered value
     tab: Vec<Vec<Expr>>,
     /// Map expressions to their corresponding value number.
-    /// Note that instances of `Expr::Result` should not be keys of this map, because it could
-    /// correspond to several values.
+    /// Note that if an expression is `Expr::Result` and the variable it holds is global, it will
+    /// not be contained in this map, because the mapping is not unique.
     num: HashMap<Expr, usize>,
 }
 
@@ -94,15 +71,15 @@ impl ValueTable {
 
     /// Add expression that already has a value, this number must be less than the `reserved`
     /// argument passed to the constructor.
-    fn add_num(&mut self, expr: Expr, num: usize) {
+    fn add_num(&mut self, num: usize, expr: Expr) {
         self.tab[num].push(expr.clone());
         self.map(expr, num)
     }
 
-    /// Map `expr` to given number, as long as it is not `Expr::Result`
+    /// Map `expr` to given number, as long as it is not `Expr::Result` of a global variable.
     fn map(&mut self, expr: Expr, num: usize) {
         match expr {
-            Expr::Result(_) => {} // mapping is not unique
+            Expr::Temp(sym) if !sym.is_local_var() => {} // never map global variables
             expr => { self.num.insert(expr, num); }
         }
     }
@@ -175,6 +152,9 @@ impl ValueTable {
             Expr::Binary(BinExpr { op, ref ty, fst, snd }) => {
                 let fst_cn = self.find_const(fst);
                 let snd_cn = self.find_const(snd);
+                if let (Some(l), Some(r)) = (fst_cn, snd_cn) {
+                    return Some(self.find_or_add(Expr::Const(op.eval(l, r))))
+                }
                 let zero = Const::zero(ty);
                 let one = Const::one(ty);
                 match op {
@@ -186,13 +166,13 @@ impl ValueTable {
                     }
                     // x - x = 0
                     BinOp::Sub if fst == snd =>
-                        Some(self.find_or_add(&Expr::Const(zero.clone()))),
+                        Some(self.find_or_add(Expr::Const(zero.clone()))),
                     // 0 * x = x * 0 = 0, 1 * x = x * 1 = x
                     BinOp::Mul => match (fst_cn, snd_cn) {
                         (Some(l), _) if l == zero =>
-                            Some(self.find_or_add(&Expr::Const(zero.clone()))),
+                            Some(self.find_or_add(Expr::Const(zero.clone()))),
                         (_, Some(r)) if r == zero =>
-                            Some(self.find_or_add(&Expr::Const(zero.clone()))),
+                            Some(self.find_or_add(Expr::Const(zero.clone()))),
                         (Some(l), _) if l == one => Some(snd),
                         (_, Some(r)) if r == one => Some(fst),
                         _ => None
@@ -232,12 +212,12 @@ impl ValueTable {
     }
 
     /// Possibly find indices for expression or create new number for it
-    fn find_or_add(&mut self, expr: &Expr) -> usize {
-        self.find(expr).unwrap_or_else(|| self.add(expr.clone()))
+    fn find_or_add(&mut self, expr: Expr) -> usize {
+        self.find(&expr).unwrap_or_else(|| self.add(expr))
     }
 }
 
-/// Implement GVN-PRE proposed by Thomas and Antony.
+/// Implement GVN-PRE proposed by Thomas.
 /// See [https://www.cs.purdue.edu/homes/hosking/papers/cc04.pdf].
 pub struct PreOpt {}
 
@@ -253,30 +233,168 @@ impl FnPass for PreOpt {
         let size = num_set.len();
         let num_remap: HashMap<usize, usize> = num_set.into_iter()
             .enumerate().map(|(new, old)| (old, new)).collect();
-        sym_num.iter_mut().for_each(|(sym, num)| *num = num_remap[num]);
+        sym_num.iter_mut().for_each(|(_, num)| *num = num_remap[num]);
 
         // Build sets as well as value table
+        let mut builder = SetBuilder {
+            sym_num,
+            table: ValueTable::new(size),
+            sets: Default::default(),
+        };
+        func.walk_dom(&mut builder);
+
+        // Build anticipated set in reverse CFG order
+        func.post_dom(|block| println!("{:?}", block));
     }
 }
 
-/// Keep record of sets used in PRE for each basic block
-struct PreSet {
-    /// Block that this structure refer to
-    block: BlockRef,
+/// Keep record of leader sets used in PRE for each basic block
+#[derive(Debug)]
+struct LeaderSet {
     /// Expressions generated by this block
-    expr: Vec<Expr>,
+    expr: HashMap<usize, Expr>,
     /// Phi destinations of this block
-    phi: Vec<SymbolRef>,
+    phi: HashMap<usize, SymbolRef>,
     /// Temporaries created in this block
-    tmp: Vec<SymbolRef>,
-    /// Available expressions at entrance and exit of this block
-    avail_in: Vec<Expr>,
-    avail_out: Vec<Expr>,
+    tmp: HashMap<usize, SymbolRef>,
+    /// Available expressions at exit of this block
+    avail_out: HashMap<usize, Expr>,
     /// Anticipated expressions at entrance and exit of this block
-    antic_in: Vec<Expr>,
-    antic_out: Vec<Expr>,
+    antic_in: HashMap<usize, Expr>,
+    antic_out: HashMap<usize, Expr>,
 }
 
+impl LeaderSet {
+    fn new() -> LeaderSet {
+        LeaderSet {
+            expr: Default::default(),
+            phi: Default::default(),
+            tmp: Default::default(),
+            avail_out: Default::default(),
+            antic_in: Default::default(),
+            antic_out: Default::default(),
+        }
+    }
+}
+
+struct SetBuilder {
+    sym_num: HashMap<SymbolRef, usize>,
+    table: ValueTable,
+    sets: HashMap<BlockRef, LeaderSet>,
+}
+
+impl BlockListener for SetBuilder {
+    fn on_begin(&mut self, func: &Func) {
+        // Add parameters to value table and available set of entrance
+        let ent = func.ent.borrow().clone();
+        let avail_in: HashMap<usize, Expr> = func.param.iter().map(|p| {
+            let sym = p.borrow().clone();
+            let num = self.sym_num[&sym];
+            self.table.add_num(num, Expr::Temp(sym.clone()));
+            (num, Expr::Temp(sym.clone()))
+        }).collect();
+        let mut leader = LeaderSet::new();
+        leader.avail_out = avail_in;
+        self.sets.insert(ent, leader);
+    }
+
+    fn on_end(&mut self, _func: &Func) {}
+
+    fn on_enter(&mut self, block: BlockRef) {
+        // Register this block
+        if !self.sets.contains_key(&block) { // not entrance block
+            let mut leader = LeaderSet::new();
+            leader.avail_out = self.sets[&block.parent().unwrap()].avail_out.clone();
+            self.sets.insert(block.clone(), leader);
+        }
+
+        // Visit instructions
+        block.instr.borrow().iter().for_each(|instr| {
+            // Skip those whose results are not stored in local variables
+            match instr.dst() {
+                None => return, // this instruction does not produce a value
+                Some(sym) if !sym.borrow().is_local_var() => return, // result stored to global
+                _ => {}
+            }
+
+            // Process destination symbol
+            macro_rules! set {
+                ($name:ident) => {self.sets.get_mut(&block).unwrap().$name};
+            }
+            let dst = instr.dst().unwrap().borrow().clone();
+            let dst_num = self.sym_num[&dst];
+            let dst_expr = Expr::Temp(dst.clone());
+
+            // Visit interested instruction
+            match instr.deref() {
+                Instr::Phi { src: _, dst: _ } => {
+                    self.table.add_num(dst_num, dst_expr.clone());
+                    Self::try_insert(&mut set!(phi), dst_num, dst);
+                    Self::try_insert(&mut set!(avail_out), dst_num, dst_expr);
+                }
+                Instr::Mov { src, dst: _ } => {
+                    let (src_num, src_expr) = self.find_src(src);
+                    Self::try_insert(&mut set!(expr), src_num, src_expr);
+                    self.table.add_num(dst_num, dst_expr.clone());
+                    Self::try_insert(&mut set!(tmp), dst_num, dst);
+                }
+                Instr::Bin { op, fst, snd, dst: _ } => {
+                    // Build expression for binary operation
+                    let (fst_num, fst_expr) = self.find_src(fst);
+                    Self::try_insert(&mut set!(expr), fst_num, fst_expr);
+                    let (snd_num, snd_expr) = self.find_src(snd);
+                    Self::try_insert(&mut set!(expr), snd_num, snd_expr);
+                    let bin_expr = Expr::Binary(BinExpr {
+                        op: *op,
+                        ty: fst.borrow().get_type(),
+                        fst: fst_num,
+                        snd: snd_num,
+                    });
+                    // Find number for expression and destination
+                    let dst_num = self.table.find(&bin_expr).unwrap_or(dst_num);
+                    self.table.add_num(dst_num, bin_expr.clone());
+                    self.table.add_num(dst_num, dst_expr);
+                    Self::try_insert(&mut set!(expr), dst_num, bin_expr);
+                    Self::try_insert(&mut set!(tmp), dst_num, dst);
+                }
+                _ => {
+                    self.table.add_num(dst_num, dst_expr.clone());
+                    Self::try_insert(&mut set!(tmp), dst_num, dst);
+                    Self::try_insert(&mut set!(avail_out), dst_num, dst_expr);
+                }
+            }
+        })
+    }
+
+    fn on_exit(&mut self, _block: BlockRef) {}
+
+    fn on_enter_child(&mut self, _this: BlockRef, _child: BlockRef) {}
+
+    fn on_exit_child(&mut self, _this: BlockRef, _child: BlockRef) {}
+}
+
+impl SetBuilder {
+    fn find_src(&mut self, opd: &RefCell<Value>) -> (usize, Expr) {
+        match opd.borrow().deref() {
+            // Local variable must have been numbered before
+            Value::Var(sym) if sym.is_local_var() => (self.sym_num[sym], Expr::Temp(sym.clone())),
+            // Must allocate new number for each use of global variable
+            Value::Var(sym) => {
+                let expr = Expr::Temp(sym.clone());
+                (self.table.add(expr.clone()), expr)
+            },
+            // Find number of existing constant or allocate new number for new constant
+            Value::Const(c) => {
+                let expr = Expr::Const(c.clone());
+                (self.table.find_or_add(expr.clone()), expr)
+            }
+        }
+    }
+
+    fn try_insert<T>(set: &mut HashMap<usize, T>, num: usize, elem: T) {
+        if !set.contains_key(&num) { set.insert(num, elem); }
+    }
+}
 
 #[test]
 fn test_pre() {
@@ -290,7 +408,7 @@ fn test_pre() {
     use std::io::Read;
     use std::borrow::BorrowMut;
 
-    let mut file = File::open("test/gvn.ir").unwrap();
+    let mut file = File::open("test/pre.ir").unwrap();
     let lexer = Lexer::try_from(&mut file as &mut dyn Read).unwrap();
     let parser = Parser::new(lexer);
     let tree = parser.parse().unwrap();
